2810c59cb4cfcce34d88c7fc6a8ac52296bf179e
SimonXinWang <simon.wang@unswalumni.com>
2023-11-24 21:03:06 +1100

    added line processing, implementation design about extracting and array to list

--- gnss-plots.py
+++ gnss-plots.py
@@ -28,17 +28,79 @@
     Description         : 
   **************************************************************************************************
 """
+# for plotting function
 import numpy as np
 import matplotlib.pyplot as plt
 
-# CRC polynomials
-CRC_UTILITY__CRC_8_CCITT_NORMAL = 0x07
-CRC_UTILITY__CRC_8_CCITT_REVERSED = 0xE0
-CRC_UTILITY__CRC_8_TH_SHT4X = 0x31
+# for data process function
+import logging as log
+import tkinter as tk
+from tkinter import filedialog as fd
+import openpyxl as xlsx
+import time
+import os
+
 
 # Error Codes
-CRC_UTILITY__ERROR_INVALID_PARAMETER = 2
+GNSS__ERROR_INVALID_PARAMETER = 2
 
+# Number of CPU cores to reserve for other tasks external to this application.
+RESERVE_CORES = 1
+
+# If parsing more than EFFICIENCY_THRESH number of log files, process using multiprocessing.
+# Otherwise if parsing less log files, it is more efficient to parse sequencially.
+EFFICIENCY_THRESH = 5
+
+# Section keys used to denote the last line of text in a log file "section" where formatting
+# is very different in the following section. These should be unique words or phrases. Tuple
+# items are in order of the section number. i.e. SECTION_KEYS[0] relates to the end of
+# the first section in the log file. The last section at the end of the log file does not need a
+# section delimiter.
+SECTION_KEYS = (" 2023", "Device Chemistry")
+SECTION_DELIMS = [(" ", ","), ("=",)]
+
+# Delimiters used to separate data and labels. Used for the parse_all function.
+# DEFAULT_DELIMS = (",", ":", " ", "=")
+DEFAULT_DELIMS = (",")
+
+# Delimiters used to isolate labels. Used for the parse_labels function.
+LABEL_DELIMS = ("-", ":")
+
+# Delimiters used to isolate labels. Used for the parse_labels function.
+DATA_DELIMS = (" ", ",")
+
+# Output .xlsx file name.
+OUTPUT_FILE_NAME = "Parsed_Logs.xlsx"
+
+
+def parse(line: str, delims: tuple) -> list:
+    """
+    Parse an individual line from a log file.
+
+    Args:
+        line: Individual line of a log file.
+        delims: Delimiters used to separate labels and data.
+
+    Returns:
+        List of parsed labels and data with leading/trailing whitespace removed.
+
+    """
+    extract =[]
+    # Replace and split is faster than regex split method.
+    for delim in delims:
+        if delim == delims[0]:
+            pass
+        else:
+            line = line.replace(delim, delims[0])
+    ret = line.split(delims[0])
+    for index , elem in ret:
+        # print(elem)
+        # if elem == '$GPGGA'
+            if index == 2 or index == 3 or index == 4 or index == 5:
+                extract.append(elem)
+    # [x.strip() for x in ret]
+    return extract
+    # x.strign remove any trailnig white space
 
 # @brief    Function to calculate CRC8 checksum of given data, least significant bit first,
 #           both in terms of the algorithm and the polynomial to be used,
@@ -49,8 +111,44 @@ CRC_UTILITY__ERROR_INVALID_PARAMETER = 2
 # @return   error           - error result of funciton
 # @return   crc_accumulator - CRC value result
 #
+def parse_all(log_file=None, delims=None, section_keys=None):
+    """
+    Parse everything in a log file. return a list of lists where each "sub-list"
+    represents a parsed line in the log file.
 
-# Data for plotting
+    Args:
+        log_file: Full path of the log file to be parsed.
+        delims: Tuple conaining delimiter characters. If multiple sections, this will be a list
+            of tuples.
+        section_keys: Tuple containing unique words or phrases that denote the last line of a
+            section within the log file.
+
+    Returns:
+        List of lists where each "sub-list" represents a parsed line in the log file.
+
+    """
+    ret = []
+    with open(log_file, 'r') as lf:
+        file = lf.readlines()
+        if section_keys is None:
+            # No separate sections.
+            for line in file:
+                ret.append(parse(line, delims))
+        else:
+            # Parse each section of the log file separately.
+            section_num = 0
+            for line in file:
+                ret.append(parse(line, delims[section_num]))
+                if section_num < len(section_keys):
+                    if section_keys[section_num] in line:
+                        section_num += 1
+    return ret
+
+
+
+# generating dummy Data for plotting 
+
+# np.array(ret)
 t = np.arange(0.0, 2.0, 0.01)
 s = 1 + np.sin(2 * np.pi * t)
 
@@ -71,40 +169,40 @@ plt.show()
 # CRC calc for TempHumidity Sensor samples
 #
 print("CRC8 for real samples")
-error, crc = calculate_crc8_lsb([0x10, 0xC5], CRC_UTILITY__CRC_8_TH_SHT4X)
-print(hex(crc))
-print("error is:", error)
-
-
-print("CRC8 for example")
-error, crc = calculate_crc8_lsb([0xEF, 0xBF], CRC_UTILITY__CRC_8_TH_SHT4X)
-print(hex(crc))
-error, crc = calculate_crc8_lsb([0xBF, 0xEF], CRC_UTILITY__CRC_8_TH_SHT4X)
-print(hex(crc))
-error, crc = calculate_crc8_lsb(
-    [0xBF, 0xEF], CRC_UTILITY__CRC_8_CCITT_REVERSED)
-print(hex(crc))
-
 print("created my own exercise Git repository for practice python")
 print("---------------------------------------------")
-#
-# verify CRC result by inserting the result into end of data array
-#
-print(calculate_crc16([0, 1, 2, 1, 0, 1, 0, 0, 0xC61B]))
-
-print("Simon tried himself here")
-print(hex(calculate_crc16([0x00, 0x01, 0x02, 4, 0, 1, 0, 0])))
 
 
-#
-# all data are in Hex
-#
-print('Hex of Caclulated CRC16:', hex(calculate_crc16(
-    [0x7E, 0x10, 0x00, 0x01, 0x02, 0x01, 0x00, 0x01, 0x00, 0x00, 0x1B, 0xC6])))
+if __name__ == '__main__':
+    log.basicConfig(level=log.DEBUG)
 
-#
-# all data are in decimal
-#
-print('Caclulated CRC16 in decimal:', calculate_crc16(
-    [0, 1, 2, 1, 0, 1, 0, 0, 125, 1782]))
+    root = tk.Tk()
+    root.withdraw()
+    folder = fd.askdirectory(title="Choose a folder containing log files")
 
+    if not folder:
+        log.info("No folder selected.")
+        time.sleep(2)
+        quit()
+    
+    # os.path.directoryname
+    files = os.walk(folder)
+    log_files = []
+    results = []
+
+    for root, directories, filenames in os.walk(folder):
+        for filename in filenames:
+            if filename.endswith(".log") or filename.endswith(".txt"):
+                log.debug(filename)
+                log_files.append(os.path.join(root, filename))
+
+        log.info(f"Number of log files to parse: {len(log_files)}")
+
+    # Based on number of files to be parsed, determine the most efficient processing method.
+    start_time = time.time()
+    for file in log_files:
+        results.append(parse_all(file, DEFAULT_DELIMS))
+
+
+
+#turn py to exe pyinstaller
