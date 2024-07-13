5dd65643546dc1c8f216531991e0474c8e16b6d3
SimonXinWang <simon.wang@unswalumni.com>
2024-07-13 10:57:55 +1000

    kept only my code outside office hours

--- crc_utility.py
+++ crc_utility.py
@@ -1,33 +1,8 @@
 """
   **************************************************************************************************
-  * @file    crc_utility.py
-  * @author  Genesys Electronics Design Team
-  * @version V1.0.0
-  * @date    14-Apr-21
   * @brief   This module is used for performing the cyclic redundancy check algorithm.
   *
   @verbatim
-  **************************************************************************************************
-   Copyright (c) 2020, Genesys Electronics Design Pty Ltd
-   All Rights Reserved
-   Unit 5/33 Ryde Road
-   Pymble NSW 2073
-   Australia
-   Telephone # +61-2-9496 8900
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
-   IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
-   FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-   DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-   DATA, OR PROFITS OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
-   IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
-   OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  **************************************************************************************************
-    Revision Number     : 1.0.0
-    Revision By         : MC
-    Date                : 14-Apr-21
-    Description         : This module is used for performing the cyclic redundancy check algorithm
   **************************************************************************************************
 """
 
@@ -49,58 +24,7 @@ CRC_UTILITY__ERROR_INVALID_PARAMETER = 2
 # @return   error           - error result of funciton
 # @return   crc_accumulator - CRC value result
 #
-def calculate_crc8_lsb(data_ro, crc_polynomial):
-    error = 0
-    byte_counter = 0
-    # original
-    crc_accumulator = 0       
-    # crc_accumulator = 0xFF
 
-    #check inputs data not empty
-    if ((not data_ro) or (not crc_polynomial)):
-        error = CRC_UTILITY__ERROR_INVALID_PARAMETER
-        print('Invalid Data or CRC Accumulator.')
-    else:
-        if ((len(data_ro) == 0) or (crc_polynomial == 0)):
-            error = CRC_UTILITY__ERROR_INVALID_PARAMETER
-            print('Invalid Length or Polynomial.')
-        else:
-            byte_counter = 0
-
-            for _ in range(len(data_ro)):
-                data_byte = data_ro[byte_counter]
-                crc_accumulator = (crc_accumulator ^ data_byte)
-                crc_accumulator &= 0xFF
-                for _ in range(8):
-                    # if rightmost (most significant) bit is set
-                    if crc_accumulator & 0x01 != 0:
-                        # (right shift by 1) XOR polynomial
-                        crc_accumulator = crc_polynomial ^ (crc_accumulator >> 1)
-                    else:
-                        # right shift by 1
-                        crc_accumulator = (crc_accumulator >> 1)
-                    crc_accumulator &= 0xFF
-                byte_counter = byte_counter + 1
-
-    return error, crc_accumulator
-
-def calculate_crc16(data_ro):
-    crc_accumulator = 0
-    byte_counter = 0
-    for _ in range(len(data_ro)):
-        data_byte = data_ro[byte_counter]
-        crc_accumulator = crc_accumulator ^ data_byte
-        crc_accumulator &= 0xFFFF
-        crc_polynomial = 0x8408
-        for _ in range(8):
-            if (crc_accumulator & 0x1) != 0:
-                crc_accumulator = crc_polynomial ^ (crc_accumulator >> 1)
-            else:
-                crc_accumulator = crc_accumulator >> 1
-            crc_accumulator &= 0xFFFF
-        byte_counter = byte_counter + 1
-
-    return crc_accumulator
 
 
 #
